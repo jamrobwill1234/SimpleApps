b08869c29ae413c0e19844542122fc7dbe305baa
James W <jamrobwill1234@gmail.com>
2015-05-26 05:40:24 -0400

    Calculator with explanations

--- CalculatorFuctions/CalculatorFuctions/ViewController.swift
+++ CalculatorFuctions/CalculatorFuctions/ViewController.swift
@@ -9,74 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
+ // new class ViewController
+    
     var firstNumber = ""
     var secondNumber = ""
     
     var currentOperation = ""
     
-    
+    // each variable will be set as a string but is not set just yet
     
     
     
     @IBOutlet weak var displayLabel: UILabel!
-    
+    // this is an outlet for the variable display label which connects the code to the output of the calculator
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // viewdidload is used for customizing the appearence of a view. It means that all of your IBoutlets are ready and connected
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // deals with your memory.... default
         
         
         
     }
     @IBAction func pressedButton1(sender: UIButton)
+        // this is an action that connectes all of your number buttons it means that when a button is pushed the event is sent
         {
         
             let number = sender.currentTitle!
+            // this means that the number will stay the same as the current title that is displayed on the button
         
         if currentOperation.isEmpty {
     
     firstNumber += number
-        
+        // this means when there is nothing in the calculator field then the first number should show
             displayLabel.text = "\(firstNumber)"
             
         } else {
             secondNumber += number
             
             displayLabel.text = "\(secondNumber)"
+            // this means that even when there is already a first number it should only show the second number selected
             
         }
         // println("Pressed Button " + sender.currentTitle!)
     }
     
-    // ASKJO about how to take numbers back out how to break connections
     
     @IBAction func operatorPressed(sender: UIButton) {
-        
+        // this is the control that sends the event when the secondary buttons (=,.,+ etc) are selected
         let operation = sender.currentTitle!
         
         if operation != "=" { currentOperation = operation }
         else {
-            
+        // if the operation is not equal to the equal button then keep the current operation??
             
             switch currentOperation {
             
             case "+":
                 
                 let value = firstNumber.toInt()! + secondNumber.toInt()!
-                
+                // + (addition sign) let the value equal the first number added to the second
                
                 println("\(firstNumber) \(currentOperation) \(secondNumber) = \(value)")
               displayLabel.text = "\(value)"
+                // this just prints out the function in the field below so you can see the operation
                 
-                case "-":
+            case "-":
                     
-                //do subtraction
+                // this means that the first number as an integer subtracted by the second number will give you your value
                 let value = firstNumber.toInt()! - secondNumber.toInt()!
                 println("\(firstNumber) \(currentOperation) \(secondNumber) = \(value)")
                 displayLabel.text = "\(value)"
@@ -86,7 +91,8 @@ class ViewController: UIViewController {
             case "/":
                 
                 let value = Double(firstNumber.toInt()!) / Double(secondNumber.toInt()!)
-
+                // for division a double is a Uint 64 ?? not sure why its used
+                
                 println("\(firstNumber) \(currentOperation) \(secondNumber) = \(value)")
                 
                 displayLabel.text = "\(value)"
@@ -94,6 +100,7 @@ class ViewController: UIViewController {
                 
             
             case "*" :
+                // setting the value as the first number multiplied by the second
                 let value = firstNumber.toInt()! * secondNumber.toInt()!
                  println("\(firstNumber) \(currentOperation) \(secondNumber) = \(value)")
                 
@@ -103,7 +110,7 @@ class ViewController: UIViewController {
                 
                 
             default :
-                // blah
+                // set a default so it would show blahh instead of crash
                 println("blah")
             }
             
--- MyWorld/MyWorld/ViewController.swift
+++ MyWorld/MyWorld/ViewController.swift
@@ -9,11 +9,18 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class MyAnnotation : MKPointAnnotation {
+    
+    var venueIndex: Int!
+}
+
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     @IBOutlet weak var myMapView: MKMapView!
     
     var lManager = CLLocationManager()
+    
+    var allVenues: [[String:AnyObject]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +30,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         myMapView.showsUserLocation = true
         myMapView.userTrackingMode = .Follow
+        myMapView.delegate = self
         
         lManager.startUpdatingLocation()
         
@@ -38,7 +46,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 
                 println(venues)
                 
-                for venue in venues as! [[String:AnyObject]] {
+                
+                self.allVenues = venues as! [[String:AnyObject]]
+                
+                for (index,venue) in enumerate(venues as! [[String:AnyObject]]) {
+                    
+                    
                     
                     if let locationInfo = venue["location"] as? [String:AnyObject] {
                         
@@ -48,9 +61,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                                 
                                 let coordinate = CLLocationCoordinate2DMake(lat, lng)
                                 
-                                let annotation = MKPointAnnotation()
-                                
+                                let annotation = MyAnnotation()
+                    
                                 annotation.title = venue["name"] as? String
+                                annotation.venueIndex = index
                                 
                                 annotation.coordinate = coordinate
                                 
@@ -63,6 +77,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                     }
                     
                 }
+            
             })
             
             lManager.stopUpdatingLocation()
@@ -108,7 +123,47 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 
             })
             
+        
         }
+        
+    }
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        
+        if annotation is MKUserLocation { return nil }
+        
+        var annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "annotation")
+        annotationView.canShowCallout = true
+        
+        annotationView.pinColor = MKPinAnnotationColor.Green
+        
+        var button = UIButton.buttonWithType(UIButtonType.DetailDisclosure) as! UIButton
+        annotationView.rightCalloutAccessoryView = button
+        
+        button.tag = (annotation as! MyAnnotation).venueIndex
+        
+        button.addTarget(self, action: "showMoreInfo:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        return annotationView
+    }
+    
+    func showMoreInfo(sender: UIButton) {
+        
+        
+       var detailVC = UIViewController()
+        
+       var venue = allVenues[sender.tag]
+
+        detailVC.view.backgroundColor = UIColor.whiteColor()
+        
+       detailVC.navigationItem.title = venue["name"] as? String
+        var box = UIView(frame: CGRectMake(20, 20, 100, 100)
+        
+        box.backgroundColor = UIColor.blueColor()
+            
+       self.navigationController?.pushViewController(detailVC, animated: true)
+        
+    
         
     }
 }
